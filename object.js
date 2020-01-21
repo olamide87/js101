19024a123ee133845c79102fef1e322356f37b68
Olamide Oladinni <olajide1987@gmail.com>
2020-01-20 19:40:14 -0600

    add changes

---
+++ object.js
@@ -0,0 +1,21 @@
+console.log('less is more');
+
+const wordLength = () => {
+    return word.length;
+};
+
+console.log('num of letters', wordLength('cat'));
+console.log('num of letter', wordLength ('monkeybutt'));
+
+const ageCheck = () => {
+    if(age >= 21){
+        console.log('party!!!!!!');
+    } else {
+        console.log('no driks for you');
+    }
+
+};
+
+ageCheck(12);
+ageCheck(24);
+
