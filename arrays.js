aae4f58ea1e424aa2ac0515ed35b34e1c3785f78
Olamide Oladinni <olajide1987@gmail.com>
2020-01-20 21:05:44 -0600

    arrays

---
+++ arrays.js
@@ -0,0 +1,14 @@
+console.log("ARRAYS");
+
+const junkDrawer = ['hi ']
+
+
+const valuePrinter = (arr, idx) => {
+    return arr[idx];
+
+};
+console.log(valuePrinter([1,2,3,4,5],2));
+console.log(valuePrinter(['cat','dog','bear','turtle','fish'],3));
+
+
+
